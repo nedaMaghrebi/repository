5035b34165f874df717975711c29383fe009d82e
neda566 <116642658+neda566@users.noreply.github.com>
2022-12-13 10:30:43 +0200

    first commit

---
+++ CarClass/src/package1/CarClass.java
@@ -0,0 +1,86 @@
+package package1;
+
+public class CarClass {
+
+
+		  //variable
+
+		  public  String brand;
+
+		  public  String model;
+
+		  public  int amountOfFuel;
+
+		  //Methods 
+
+		  public void brake() {
+
+		    // method for breaking.
+
+		    System.out.println("Car is breaking.");
+
+		  }
+
+		   public int accelerate() {
+
+		    if (amountOfFuel > 0) {
+
+		      System.out.println("Car is accelerating");
+
+		      amountOfFuel =amountOfFuel-1;
+
+		      return amountOfFuel;
+
+		    }
+
+		    return amountOfFuel;
+
+		   }
+
+		    
+
+		    public void printdata() {
+
+		    System.out.println("brand :"+ brand );
+
+		    System.out.println("model :" + model );
+
+		    System.out.println("amountOfFuel:" + amountOfFuel);
+
+		  }
+
+		    public void refuel( int refuel) {
+
+		      refuel = amountOfFuel + refuel;
+
+		      System.out.println("Fuel on the tank after the refuel:" +refuel);
+
+		    }
+
+		  public CarClass() {
+
+		       brand ="";
+
+		       model="";
+
+		       amountOfFuel=0;
+
+		  }
+
+		  public CarClass(String brand,String model, int amountOfFuel ) {
+
+		       this.brand =brand;
+
+		       this.model=model;
+
+		       this.amountOfFuel=amountOfFuel;
+
+		  }
+
+		  
+
+		  
+
+		}
+
+
